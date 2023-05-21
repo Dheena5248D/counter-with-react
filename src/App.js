0ca5f70f6e4f58be016bf8c15be18134b766737e
dheena <dheenadhayalan5248d@gmail.com>
2023-05-21 21:11:01 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount ] = useState(0);
+ const funinc = () =>{
+    setCount(count +1)
+  }
+function fundec(){
+setCount(count -1)
 }
-
+function funres(){
+setCount(0)
+}
+  return(
+    <div>
+    <div id="main">
+    <div id ="display">
+    {count}</div>
+    <div id ="buttons">
+    <button onClick={funinc}>+</button>
+    <button onClick={funres}><span className="reload">&#x21bb;</span></button>
+    <button onClick={fundec}>- </button>
+   </div></div></div>
+  )
+  }
 export default App;
+
+
+
+
+
